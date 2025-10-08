7fc2d0487f47675b2b0eb941e954ae57c4778d6c
virgilio fuchs <indiozinn008@gmail.com>
2025-10-08 09:17:31 -0300

    Fundamentos do Java!

--- src/main/java/org/example/NotasAlunos.java
+++ src/main/java/org/example/NotasAlunos.java
@@ -1,9 +1,9 @@
 package org.example;
 
-//Solicite ao usuário as notas de um aluno (entre 0 e 10).
+//Solicite ao usuário as notas de um aluno (entre 0 e 10). OK
 //Valide se cada nota é válida (não aceitar valores fora desse intervalo). OK
 //Calcule a média e informe se o aluno foi Aprovado, Recuperação ou Reprovado. OK
-//Permita repetir o processo para vários alunos até o usuário digitar “sair”.
+//Permita repetir o processo para vários alunos até o usuário digitar “sair”. OK
 
 import java.util.Objects;
 import java.util.Scanner;
@@ -15,23 +15,27 @@ public class NotasAlunos {
     public static void main(String[] args) {
         String nome = null;
         String resposta = null;
-        double nota = 0.0;
-        int materias = 0;
-
-        System.out.println("Digite seu nome: ");
-        nome = entrada.nextLine();
-
-        double notas = 0.0;
+        double nota1 = 0.0;
+        double nota2 = 0.0;
+        double nota3 = 0.0;
 
         do {
-            nota = validarNota("Digite sua nota: (Ex: 10.0)");
-            notas += nota;
-            materias++;
+            System.out.println("Digite seu nome: ");
+            nome = entrada.nextLine();
+
+            nota1 = validarNota("Digite sua nota 1: (Ex: 10.0)");
+            nota2 = validarNota("Digite sua nota 2: (Ex: 10.0)");
+            nota3 = validarNota("Digite sua nota 3: (Ex: 10.0)");
+
+            System.out.println(resultadoMedia(nota1, nota2, nota3, nome));
             System.out.println("Deseja continuar ou sair? Digite 'sair' se quiser sair");
             resposta = entrada.nextLine();
-        } while (!Objects.equals(resposta, "sair"));
+            System.out.println("------------------------------");
 
-        System.out.println(resultadoMedia(notas, materias, nome));
+        } while (!resposta.equalsIgnoreCase("sair"));
+
+        System.out.println("Saindo!!");
+        entrada.close();
     }
 
     public static double validarNota(String mensagem) {
@@ -39,18 +43,30 @@ public class NotasAlunos {
             System.out.println(mensagem);
             String input = entrada.nextLine();
 
-            double resultado = Double.parseDouble(input);
-            if (resultado >= 0 && resultado <= 10) {
-                return resultado;
-            } else {
-                System.out.println("Digite uma nota valida");
+            try {
+                double resultado = Double.parseDouble(input);
+                if (resultado >= 0 && resultado <= 10) {
+                    return resultado;
+                } else {
+                    System.out.println("Digite uma nota valida de 0 a 10");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Digite uma nota valida em número");
             }
         }
     }
 
-    public static String resultadoMedia(double nota, int materias, String nome) {
-        double resultado = nota / materias;
-        String resultadoFinal = resultado >= 7.0 ? " foi aprovado" : " foi reprovado";
+    public static String resultadoMedia(double nota1, double nota2, double nota3, String nome) {
+        String resultadoFinal = null;
+        double resultado = (nota1 + nota2 + nota3) / 3.0;
+
+        if (resultado >= 7) {
+            resultadoFinal = "foi aprovado";
+        } else if (resultado >= 5) {
+            resultadoFinal = "esta de recuperacao";
+        } else {
+            resultadoFinal = "foi reprovado";
+        }
         return String.format("%s vc %s com %.1f de nota", nome, resultadoFinal, resultado);
     }
 }
